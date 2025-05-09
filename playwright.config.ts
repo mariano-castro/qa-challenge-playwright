9d1e6927c14c6d70936225063ba79077738c6f4b
Mariano Castro <marianoecastro@hotmail.com>
2025-05-09 02:20:50 -0300

    config and structure

---
+++ playwright.config.ts
@@ -0,0 +1,26 @@
+// playwright.config.ts
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000
+  },
+  retries: 0,
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 0,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry',
+    baseURL: 'https://www.demoblaze.com'
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { browserName: 'chromium' }
+    }
+  ]
+});
